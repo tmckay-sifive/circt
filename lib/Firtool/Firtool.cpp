8eacb1539f4c6a3274e1d1c3b12c2f14f4e65a7a
Schuyler Eldridge <schuyler.eldridge@sifive.com>
2025-07-07 17:20:28 -0400

    [Firtool] Add InstanceInfo analysis before Lint
    Change the Firtool pass pipeline to add the InstanceInfo analysis caching
    pass before lint.  This is done in preparation to make the lint pass use
    the cached _parent_ InstanceInfo analysis.
    
    Signed-off-by: Schuyler Eldridge <schuyler.eldridge@sifive.com>

--- lib/Firtool/Firtool.cpp
+++ lib/Firtool/Firtool.cpp
@@ -294,6 +294,7 @@ LogicalResult firtool::populateLowFIRRTLToHW(mlir::PassManager &pm,
   pm.nest<firrtl::CircuitOp>().addPass(om::createVerifyObjectFieldsPass());
 
   // Check for static asserts.
+  pm.nest<firrtl::CircuitOp>().addPass(firrtl::createAnalysisInstanceInfo());
   pm.nest<firrtl::CircuitOp>().nest<firrtl::FModuleOp>().addPass(
       circt::firrtl::createLintingPass());
 
