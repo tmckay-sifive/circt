6abddfcc44ee46cf9fb7e4ebb1bcb6c6ec821834
Schuyler Eldridge <schuyler.eldridge@sifive.com>
2025-07-07 23:08:36 -0400

    [FIRRTL] Add lint-static-asserts option to linter
    Make the linting of static assertions an option to the linter.  While this
    appears unnecessary now, I am intending to add additional lint checks to
    the linter and I want to be able to turn each of them off if they become
    problematic.
    
    Signed-off-by: Schuyler Eldridge <schuyler.eldridge@sifive.com>

--- lib/Dialect/FIRRTL/Transforms/Lint.cpp
+++ lib/Dialect/FIRRTL/Transforms/Lint.cpp
@@ -23,13 +23,21 @@ using namespace circt;
 using namespace firrtl;
 
 namespace {
+/// Linter configuration.
+struct Config {
+  /// If true, then assertions that are statically false (and will trivially
+  /// fail simulation) will result in an error.
+  bool lintStaticAsserts;
+};
+
 /// Class that stores state related to linting.  This exists to avoid needing to
 /// clear members of `LintPass` and instead just rely on `Linter` objects being
 /// deleted.
 class Linter {
 
 public:
-  Linter(FModuleOp fModule) : fModule(fModule){};
+  Linter(FModuleOp fModule, const Config &config)
+      : fModule(fModule), config(config){};
 
   /// Lint the specified module.
   LogicalResult lint() {
@@ -38,7 +46,7 @@ public:
       if (isa<WhenOp>(op))
         return WalkResult::skip();
       if (isa<AssertOp, VerifAssertIntrinsicOp>(op))
-        if (checkAssert(op).failed())
+        if (config.lintStaticAsserts && checkAssert(op).failed())
           failed = true;
 
       return WalkResult::advance();
@@ -52,6 +60,7 @@ public:
 
 private:
   FModuleOp fModule;
+  const Config &config;
 
   LogicalResult checkAssert(Operation *op) {
     Value predicate;
@@ -86,8 +95,10 @@ private:
 };
 
 struct LintPass : public circt::firrtl::impl::LintBase<LintPass> {
+  using LintBase::lintStaticAsserts;
+
   void runOnOperation() override {
-    if (failed(Linter(getOperation()).lint()))
+    if (failed(Linter(getOperation(), {lintStaticAsserts}).lint()))
       return signalPassFailure();
 
     markAllAnalysesPreserved();
@@ -95,6 +106,8 @@ struct LintPass : public circt::firrtl::impl::LintBase<LintPass> {
 };
 } // namespace
 
-std::unique_ptr<Pass> firrtl::createLintingPass() {
-  return std::make_unique<LintPass>();
+std::unique_ptr<Pass> firrtl::createLintingPass(bool lintStaticAsserts) {
+  auto pass = std::make_unique<LintPass>();
+  pass->lintStaticAsserts = lintStaticAsserts;
+  return pass;
 }
